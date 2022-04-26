f3e6c0cf33ee4ea105881c878fd80e3a3c59fed1
NazrulChowdhury <nazrulchowdhury404@gmail.com>
2022-04-26 04:29:16 +0100

    code highlighting added

--- client/src/component/pages/Post.js
+++ client/src/component/pages/Post.js
@@ -3,6 +3,7 @@ import React, { useEffect } from 'react'
 import { useQuery } from 'react-query'
 import { useParams } from 'react-router-dom'
 import { getPostById } from '../../functions/api'
+import Highlight from 'react-highlight'
 
 const Post = () => {
     const {postId} = useParams()
@@ -10,15 +11,19 @@ const Post = () => {
         enabled : false,
         onError : (error) => message.error(error.response.data)
     })
-    
+
     useEffect(() => refetch(),[])
 
   return (
     <div> 
         {data && 
            <div>
-               <b>{data.postTitle}</b> <br />
-               <p>{data.postDescription}</p>
+               <p style={{fontSize : '2rem'}}>{data.postTitle}</p> <br />
+               <div style={{whiteSpace: 'pre-wrap'}}>
+                   <Highlight className = 'css'>
+                        {data.postDescription}
+                   </Highlight>
+                </div>
            </div>
         }
     </div>
