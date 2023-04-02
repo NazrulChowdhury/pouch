aa5b31c675cf7d3ea2d4977925325e179e7f37d8
naz <nazrulchowdhury404@gmail.com>
2023-04-02 13:39:27 +0100

    added error test

--- server/src/controllers/__test__/post.controller.test.ts
+++ server/src/controllers/__test__/post.controller.test.ts
@@ -3,23 +3,22 @@ import { singlePostDocument } from "@fixtures"
 import * as PostService from '@services/post.service'
 
 describe('Post.controller test suit', () => {
+
     describe('createNewPostHandler',() => {
+
+        const {userId, _id, ...postInput} = singlePostDocument
+        const req = {
+            user : userId,
+            body : postInput
+        }
+        const res ={ send : jest.fn() }
+        const next = jest.fn() 
+
         it('Given Post input and userId provided, should return "success!" string', async() => {
-            const {userId, _id, ...postInput} = singlePostDocument
             const mockResource = jest
-            .spyOn(PostService, 'createNewPost')
-            // @ts-ignore
-            .mockResolvedValue(singlePostDocument)
-            const req = {
-                user : userId,
-                body : postInput
-            }
-            const res ={
-                send : jest.fn()
-            }
-            const next = {
-                next : jest.fn()
-            }
+                .spyOn(PostService, 'createNewPost')
+                // @ts-ignore
+                .mockResolvedValue(singlePostDocument)
             // @ts-ignore
             await createNewPostHandler(req, res, next)
 
@@ -27,9 +26,23 @@ describe('Post.controller test suit', () => {
                 .toBeCalled()
                 .toHaveBeenCalledTimes(1)
                 .toHaveBeenCalledWith("success!")
-            expect(mockResource).toHaveBeenCalled()
-            expect(mockResource.mock.calls[0][0]).toEqual(postInput)
-            expect(mockResource.mock.calls[0][1]).toBe(userId)
+            expect(mockResource)
+                .toHaveBeenCalled()
+            expect(mockResource.mock.calls[0][0])
+                .toEqual(postInput)
+            expect(mockResource.mock.calls[0][1])
+                .toBe(userId)
+        }),
+        it('Given createNewPost service throws, the next function is called with error', async()=>{
+            const mockResource = jest
+                .spyOn(PostService, 'createNewPost')
+                // @ts-ignore
+                .mockRejectedValue('error has ocurred!')
+            // @ts-ignore
+            await createNewPostHandler(req, res, next)
+            expect(next)
+                .toHaveBeenCalledTimes(1)
+                .toHaveBeenCalledWith('error has ocurred!')
         })
     })
 })
