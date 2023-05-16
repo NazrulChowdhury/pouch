2fd408977f9fb25a7f20ba4c95f60f4c58e043a1
naz <nazrulchowdhury404@gmail.com>
2023-05-16 22:03:54 +0100

    refectored controller

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -3,16 +3,16 @@ import { getUserById } from "../services/user.service"
 import { ApiError } from "@error/ApiError"
 
 export const getSession = async(req:Request, res:Response, next:NextFunction)=>{ 
-    if(req.user){
-        try{
-           const user = await getUserById(req.user as string) 
-           if(!user) throw ApiError.badRequest('Invalid user ID, No user found!')
-           const {name, picture, email} = user
-           res.send({name, picture, email})
-        } catch(error){
-            next(error)
-        }
-    } else{ 
+    if(!req.user){
         res.send(null)
+        return
+    }
+    try{
+        const user = await getUserById(req.user as string) 
+        if(!user) throw ApiError.badRequest('Invalid user ID, No user found!')
+        const {name, picture, email} = user
+        res.send({name, picture, email})
+    } catch(error){
+        next(error)
     }
 }
